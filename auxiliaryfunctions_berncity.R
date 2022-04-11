8dbb93df3fa4827f4f4666ac64ab9f3f29be17c7
codicolus <ch.vonmatt@gmx.ch>
2022-04-11 15:19:22 +0200

    licence infos changed

--- auxiliaryfunctions_berncity.R
+++ auxiliaryfunctions_berncity.R
@@ -10,26 +10,16 @@
 #  ----------------------------------------------------------------------------
 #
 #  Created by: Christoph von Matt (Twitter @chvonmatt / Github: @codicolus)
-#  Date: 27-03-2022 (cleaned + extended version)
-#
-#  Licence: MIT Licence
+#  Date: 11-04-2022 (cleaned + extended version)
 #
 #  Copyright 2022 Christoph von Matt
+#  Licence: CC-BY-NC-SA 3.0
 #
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-#  software and associated documentation files (the "Software"), to deal in the Software 
-#  without restriction, including without limitation the rights to use, copy, modify, merge, 
-#  publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to 
-#  whom the Software is furnished to do so, subject to the following conditions:
+#  For licence information see here:
+#  https://creativecommons.org/licenses/by-nc-sa/3.0/de/
+#  Full licence informations:
+#  https://creativecommons.org/licenses/by-nc-sa/3.0/de/legalcode
 #
-#  The above copyright notice and this permission notice shall be included in all copies or substantial 
-#  portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
-#  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-#  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 ###############################################################################################
 
@@ -135,14 +125,6 @@ merge_and_load <- function(paths, type = "DEM", projection = LV95_proj4){
     rm(data)
   }
   return(data_merged)
-}
-
-
-
-# Cropping to Custom
-# -------------------
-check_validBoundingValue <- function(userinput, current, extent, bbox = which_bbox){
-  
 }
 
 
